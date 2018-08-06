d689e18048c445a0a8e7346cd948578f8ce9a8ed
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-06 17:04:42 -0400

    completed homework

--- homework.js
+++ homework.js
@@ -1,36 +1,56 @@
 // 1. Set the variable `givenName` to the string "Addison".
-
+var givenName = "Addison"
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
-
+var candies = 20
+var people = 6
+var leftover = candies % people
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
-
+function greeting(name) {
+    return "Hello, " + name + "!"
+}
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not.
-
+function isOdd(num) {
+    return (Math.round(num)%2 !== 0)
+}
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not.
-
+function isEven(num) {
+    return (Math.ceil(num)%2 === 0)
+}
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
-
+function fahrenheitToCelsius(temp){
+    return (temp-32)*5/9
+}
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
-
+function celsiusToFahrenheit(temp){
+    return 1.8*temp + 32
+}
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Kelvin. This function must use your previous
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
-
+function fahrenheitToKelvin(temp) {
+    return fahrenheitToCelsius(temp) + 273.15
+}
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
-
+function lesser(x,y){
+    if (x<y){
+        return x
+    } else {
+        return y
+    }
+}
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
 // in the specified language. The supported languages and their
@@ -42,9 +62,56 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
+function multigreeting(name, language) {
+    if (language === "en") {
+        return "Hello, " + name + "!"
+    } else if (language === "es"){
+        return "¡Hola, " + name + "!"
+    }
+    else if (language === "fr"){
+        return "Bonjour, " + name + "!"
+    }
+    else if (language === "eo"){
+        return "Saluton, " + name + "!"
+    } else {
+        return;
+    }
 
+}
 // 10. Create a function, howOld, that takes a birthdate and a another
 // date (the "current date") and returns how many years old the person
 // with that birthdate was on that date. The dates should be JavaScript
 // Date objects.
 // If the birth date is after than the current date, return nothing.
+// function howOld(birth, current) {
+//     if (birth.getTime() === current.getTime()){
+//         return current.getFullYear() - birth.getFullYear()
+//      }
+//      else if (birth.getTime() >= current.getTime()){
+//         return current.getFullYear() - birth.getFullYear() 
+//      }
+//      else if (birth.getTime() < current.getTime()){
+//         return current.getFullYear() - birth.getFullYear() -1
+//      } else {
+//        return
+//      }
+  
+//   }
+function howOld(birth, current) {
+    var dateToMilliseconds = current - birth;
+    var year = dateToMilliseconds/1000/60/60/24/365
+    var date1 = birth.getMonth() + birth.getDate()
+    // return date1
+    var date2 = current.getMonth() + current.getDate()
+    // return date2
+    if (date2 - date1 < 0){
+    return Math.floor(year -1)
+    } else {
+      return Math.floor(year)
+    }
+    
+    }
+    
+    
+
+
